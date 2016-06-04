c5d44009bcde6e1f5c120b50df9a6032b10a54d9
Joost <joost.schermers@gmail.com>
2016-06-04 20:57:13 +0200

    update footer, googlemap, blockquote

--- src/static/javascripts/app/ui/googlemap.js
+++ src/static/javascripts/app/ui/googlemap.js
@@ -1,34 +1,34 @@
 'use strict';
 
 ddc.api.store = {
-	elm : []
+	elm: []
 };
 
-ddc.component.GoogleMap = function(element) {
+ddc.component.GoogleMap = function (element) {
 	var obj = {};
 
-	if (element) {		
+	if (element) {
 		obj.id = element.getAttribute('id');
 		obj.lat = Number(element.getAttribute('data-lat'));
 		obj.lon = Number(element.getAttribute('data-lon'));
 		obj.title = element.getAttribute('data-title');
 		ddc.api.store.elm.push(obj);
 	}
-	
+
 	if (typeof google === 'object' && typeof google.maps === 'object') {
 		for (var i = 0; i < ddc.api.store.elm.length; i++) {
-			 	map = new google.maps.Map(document.getElementById(ddc.api.store.elm[i].id), {
-		    center: {lat: ddc.api.store.elm[i].lat, lng: ddc.api.store.elm[i].lon},
-		    zoom: 17,
-		    mapTypeId: google.maps.MapTypeId.MAP,
-		    heading: 90,
-		    tilt: 45
-		  });
-			 		marker = new google.maps.Marker({
-					position: {lat: ddc.api.store.elm[i].lat, lng: ddc.api.store.elm[i].lon},
-					map: map,
-					title: ddc.api.store.elm[i].title
-		});
+			map = new google.maps.Map(document.getElementById(ddc.api.store.elm[i].id));
+
+			map.setCenter({ lat: ddc.api.store.elm[i].lat, lng: ddc.api.store.elm[i].lon})
+			map.setZoom(17);
+
+			map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+
+			marker = new google.maps.Marker({
+				position: { lat: ddc.api.store.elm[i].lat, lng: ddc.api.store.elm[i].lon },
+				map: map,
+				title: ddc.api.store.elm[i].title
+			});
 		}
 	}
 };
